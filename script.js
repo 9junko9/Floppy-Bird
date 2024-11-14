84d55b618cfaf0e999d0972731c2ca50045d03be
jennifer <jennifer.rivolier@gmail.com>
2024-11-14 12:17:47 +0100

    Add anim bird

--- script.js
+++ script.js
@@ -12,14 +12,27 @@ const size = [51, 36];
 const jump = -11.5;
 const cTenth = canvas.width / 10;
 
-let index = 0;
-bestScore = 0;
-currentScore = 0;
-pipes = [];
-flight, flyHeight;
+let index = 0,
+  bestScore = 0,
+  currentScore = 0,
+  pipes = [],
+  flight,
+  flyHeight;
 
 const render = () => {
   index++;
+
+  ctx.drawImage(
+    img,
+    432,
+    Math.floor((index % 9) / 3) * size[1],
+    ...size,
+    canvas.width / 2 - size[0] / 2,
+    flyHeight,
+    ...size
+  );
+
+  flyHeight = canvas.height / 2 - size[1] / 2;
 
   window.requestAnimationFrame(render);
 };
